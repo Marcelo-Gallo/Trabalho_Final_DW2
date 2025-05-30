12d0a41126365b2959acbad15cb85d9d9549d929
Marcelo-Gallo <marceloaugustogallo@gmail.com>
2025-05-29 23:18:01 -0300

    Aula 15: Salvando dados no banco MySQL

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create']);
-
+Route::post('/events', [EventController::class, 'store']);
 
 Route::get('/contact', function () {
     return view('contact');
