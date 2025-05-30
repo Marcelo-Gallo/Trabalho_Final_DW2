a9ae2bef7223b1067e5ec3ed8798362558950437
Marcelo-Gallo <marceloaugustogallo@gmail.com>
2025-05-30 07:35:16 -0300

    Aula 19: Salvando JSON no banco de dados

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -32,6 +32,25 @@
             <label for="description">Descrição:</label>
             <textarea name="description" id="description" class="form-control" placeholder="O que vai acontecer no evento?"></textarea>
         </div>
+        <div class="form-group">
+            <label for="description">Adicione itens de infraestrutura:</label>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Cadeiras"> Cadeiras
+            </div>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Palco"> Palco
+            </div>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Cerveja Gratis"> Cerveja Grátis
+            </div>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Open Food"> Open Food
+            </div>
+            <div class="form-group">
+                <input type="checkbox" name="items[]" value="Brindes"> Brindes
+            </div>
+        </div>
+
         <input type="submit" class="btn btn-primary" value="Criar evento">
     </form>
 </div>
