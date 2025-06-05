c2c4f6a23e163aeb0148bb8944a1f77a3fbb01a4
Marcelo-Gallo <marceloaugustogallo@gmail.com>
2025-06-05 17:40:45 -0300

    Aula 25: Criando uma dashboard

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,7 +53,7 @@ class User extends Authenticatable
     ];
 
     public function events() {
-        return $this->hasMany('App\Models\User');
+        return $this->hasMany('App\Models\Event');
     }
 
     /**
