22f5af22f28c19535035fcd2065c1bb2d69445b7
Marcelo-Gallo <marceloaugustogallo@gmail.com>
2025-06-05 20:41:29 -0300

    Aula 30: Removendo presença do evento

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -14,7 +14,8 @@
                 <p class="event-city"><ion-icon name="location-outline"></ion-icon>{{ $event->city }}</p>
                 <p class="events-participants"><ion-icon name="people-outline"></ion-icon>{{ count($event->users) }}</p>
                 <p class="event-owner"><ion-icon name="star-outline"></ion-icon>{{ $eventOwner['name'] }}</p>
-                <form action="/events/join/{{ $event->id }}" method="POST">
+                @if(!$hasUserJoined)
+                    <form action="/events/join/{{ $event->id }}" method="POST">
                     @csrf
                     <a href="/events/join/{{ $event->id }}"
                         class="btn btn-primary"
@@ -24,6 +25,9 @@
                         Confirmar Presença
                     </a>
                 </form>
+                @else
+                    <p class="already-joined-msg">Você já está participando deste evento!</p>
+                @endif
                 <h3>O evento conta com:</h3>
                 <ul id="items-list">
                     @foreach ($event->items as $item)
