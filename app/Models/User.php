dc01444d58206578e1d08593cec89953f61d9ae0
Marcelo-Gallo <marceloaugustogallo@gmail.com>
2025-06-12 11:15:04 -0300

    parametro importante em falta

--- app/Models/User.php
+++ app/Models/User.php
@@ -76,6 +76,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'is_admin' => 'boolean', // Adicione esta linha
         ];
     }
 }
